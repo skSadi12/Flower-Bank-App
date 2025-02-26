c222d6d4c0191c77d3229e11f8f0de77878ea38c
skSadi12 <sksadi433@gmail.com>
2025-02-27 01:00:10 +0600

    doing js

---
+++ index.js
@@ -0,0 +1,9 @@
+let addInput = document.querySelector("#addInput")
+let addBtn  = document.querySelector("#addBtn")
+let withdrawInput = document.querySelector("#withdrawInput")
+let withdrawBtn = document.querySelector("#withdrawBtn")
+let TransactionBtn = document.querySelector("#TransactionBtn")
+let TransactionUl = document.querySelector("#TransactionUl")
+
+let balance = 0;
+let transactionHistory = [];
